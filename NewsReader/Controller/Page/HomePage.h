de4431e6a7d4976a083ca356698d07d44b85ba3a
zengying1 <945421603@qq.com>
2016-10-23 19:59:25 +0800

    整理相关文件

---
+++ NewsReader/Controller/Page/HomePage.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsReader
+//
+//  Created by Oreal51 on 16/10/23.
+//  Copyright © 2016年 Oreal51. All rights reserved.
+//
+
+#import "ZYBasePage.h"
+
+@interface HomePage : ZYBasePage
+
+
+@end
+
---
+++ NewsReader/Controller/Page/HomePage.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NewsReader
+//
+//  Created by Oreal51 on 16/10/23.
+//  Copyright © 2016年 Oreal51. All rights reserved.
+//
+
+#import "HomePage.h"
+
+@interface HomePage ()
+
+@end
+
+@implementation HomePage
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+  
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+   
+}
+
+@end
